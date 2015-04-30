63ce5a05d5569d339cf29b8b7d2fdb0965736b97
Vince Jones <vincethebutcher@gmail.com>
2015-04-30 17:44:42 -0500

    Working Version

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 var cityData = null;
 var cityHtml = null;
 var name, pop, area, flight;
+var masterArray = [];
 
 
-function displayCities(name, pop, area, flight) {
-    $(".location-container").find(".name").text("Name: " + name);
-    $(".location-container").find(".pop").text("Population: " + pop);
-    $(".location-container").find(".area").text("Area: " + area + " (Sq Mi)");
-    $(".location-container").find(".flight").text("Flight time: " + flight );
+function displayCities(name, pop, area) {
+        $(".location-container").find(".name").text("Name: " + name);
+        $(".location-container").find(".pop").text("Population: " + pop);
+        $(".location-container").find(".area").text("Area: " + area + " (Sq Mi)");
 }
-
 
 
 $(document).ready(function(){
@@ -19,27 +18,30 @@ $(document).ready(function(){
         if (cityHtml === null){
             $.get('locations.html', function(data){
                 cityHtml = data;
+
             });
         } else {
             console.log("You already have that HTML.");
         }
 
-        $(".location-container").append(cityHtml);
 
         if (cityData === null){
             $.get('data.json', function(data){
-                cityData = data;
-                console.log(cityData);
-                name = cityData.Cities[0].name;
-                pop = cityData.Cities[0].population;
-                area = cityData.Cities[0].area;
-                flight = cityData.Cities[0].flight;
+
+                    $(".location-container").append(cityHtml);
+                    cityData = data;
+                    console.log(cityData);
+                    name = cityData.Cities[0].name;
+                    pop = cityData.Cities[0].population;
+                    area = cityData.Cities[0].area;
+
+                    displayCities(name, pop, area);
             });
         } else {
             console.log("You already have data.");
         }
 
-        displayCities(name, pop, area, flight);
+
 
     });
 
