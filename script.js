fb943e66d6e07f499cddf8136febf1fdc68c8410
Vince Jones <vincethebutcher@gmail.com>
2015-04-30 17:27:43 -0500

    Initial Commit

--- script.js
+++ script.js
@@ -2,47 +2,45 @@ var cityData = null;
 var cityHtml = null;
 var name, pop, area, flight;
 
+
+function displayCities(name, pop, area, flight) {
+    $(".location-container").find(".name").text("Name: " + name);
+    $(".location-container").find(".pop").text("Population: " + pop);
+    $(".location-container").find(".area").text("Area: " + area + " (Sq Mi)");
+    $(".location-container").find(".flight").text("Flight time: " + flight );
+}
+
+
+
 $(document).ready(function(){
-
-
-
 
     $(".getInfo").on('click', function(){
 
-        if(cityData === null){
-            $.get('data.json', function(data){
-                cityData = data;
-                console.log(cityData);
-                name = cityData.Cities[0].name;
-                pop = cityData.Cities[0].population;
-                area = cityData.Cities[0].area;
-                // flight = ;
-            });
-        } else {
-            console.log("You already have data.");
-        }
-
-        if(cityHtml === null){
+        if (cityHtml === null){
             $.get('locations.html', function(data){
                 cityHtml = data;
-                console.log(cityHtml);
             });
         } else {
             console.log("You already have that HTML.");
         }
 
         $(".location-container").append(cityHtml);
-        $(".location-container").children().append("<li class='name'>Name: " + name + "</li>");
-        $(".location-container").children().append("<li class='pop'>Population: " + pop + "</li>");
-        $(".location-container").children().append("<li>Area: " + area + " (sq mi)</li>");
 
-        console.log("here" + cityData);
-        $(".location-container").find(".flightTime").text("Flight time: " + cityData.Cities[0].flight);
+        if (cityData === null){
+            $.get('data.json', function(data){
+                cityData = data;
+                console.log(cityData);
+                name = cityData.Cities[0].name;
+                pop = cityData.Cities[0].population;
+                area = cityData.Cities[0].area;
+                flight = cityData.Cities[0].flight;
+            });
+        } else {
+            console.log("You already have data.");
+        }
 
-
+        displayCities(name, pop, area, flight);
 
     });
-
-    //$(".location-container").on('click', '.getInfo',)
 
 });
